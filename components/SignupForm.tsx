6377ef436964a6a3fc6b2b403f69e4d131c8645e
Nurkanat Baisenkul <71591558+tuchaVshortah@users.noreply.github.com>
2024-11-26 14:18:47 +0500

    setup the signup page

--- components/SignupForm.tsx
+++ components/SignupForm.tsx
@@ -1,11 +1,25 @@
 'use client'
 
+import { registerUser } from '@/actions/auth'
 import { Input } from '@nextui-org/react'
 import Link from 'next/link'
+import { useFormState } from 'react-dom'
+import { string } from 'zod'
+import SubmitButton from './SubmitButton'
+
+const initState = { message: null }
 
 const SignupForm = () => {
+  const [formData, action] = useFormState<{ message: null | string }>(
+    registerUser,
+    initState
+  )
+
   return (
-    <form className="bg-content1 border border-default-100 shadow-lg rounded-md p-3 flex flex-col gap-2 ">
+    <form
+      action={action}
+      className="bg-content1 border border-default-100 shadow-lg rounded-md p-3 flex flex-col gap-2"
+    >
       <h3 className="my-4">Sign up</h3>
       <Input fullWidth size="lg" placeholder="Email" name="email" required />
       <Input
@@ -16,6 +30,8 @@ const SignupForm = () => {
         placeholder="Password"
         required
       />
+
+      <SubmitButton label={'Sign Up'}></SubmitButton>
 
       <div>
         <Link href="/signin">{`Already have an account?`}</Link>
