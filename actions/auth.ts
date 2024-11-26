530526819c7537b82547774576836dfd505bf64a
Nurkanat Baisenkul <71591558+tuchaVshortah@users.noreply.github.com>
2024-11-26 13:06:07 +0500

    create auth action

---
+++ actions/auth.ts
@@ -0,0 +1,28 @@
+'use server'
+import { cookies } from 'next/headers'
+import { signin, signup } from '@/utils/authTools'
+import { z } from 'zod'
+import { redirect } from 'next/navigation'
+import { COOKIE_NAME } from '@/utils/constants'
+
+const authSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+
+const registerUser = async (prevState: any, formData: FormData) => {
+  const data = authSchema.parse({
+    email: formData.get('email'),
+    password: formData.get('password'),
+  })
+
+  try {
+    const { token } = await signup(data)
+    cookies().set(COOKIE_NAME, token)
+  } catch (e) {
+    console.log(e)
+    return { message: 'Failed to sing you up' }
+  }
+
+  redirect('/dashboard')
+}
